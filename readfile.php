192be5e11f9521060c7eee702aeb9a4c7f41df1a
sudhir <sudhir.rupani@peaas.co>
2018-08-03 11:54:33 +0530

    mail changes

--- readfile.php
+++ readfile.php
@@ -61,8 +61,8 @@ $final_array[] = $result__array ;
   ";
     $headers = "From: pixelpro <contact@pixelpro.com>\r\n";
     $headers .= "X-Priority: 1\r\n";
-    $headers . = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8\r\n";
   mail($to,$subject,$message,$headers);
 echo "sucess"; 
 } 
